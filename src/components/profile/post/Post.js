0101fa7daad6af6221b9792250beb8cc584a6093
piy-bebe <piy.bebe@gmail.com>
2021-10-25 18:57:18 +0300

    react hooks

--- src/components/profile/post/Post.js
+++ src/components/profile/post/Post.js
@@ -1,9 +1,19 @@
+import React from 'react'
+
 function Post() {
+  const [text, setText] = React.useState('Привет мир')
+
+  function handleClick() {
+    setText('Покедаво!')
+  }
+
   return (
     <div className="card mb-3">
       <div className="card-body">
         <h5 class="card-subtitle mb-2 text-muted">Alexey Orlov</h5>
-        <p className="card-text">This is some text within a card body.</p>
+        <p onClick={handleClick} className="card-text">
+          {text}
+        </p>
       </div>
     </div>
   )
